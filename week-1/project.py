6a32d73bf2a12808d732ed78162c00516033c060
dyceszn <duruohaikenna@gmail.com>
2024-03-11 18:37:25 +0100

    add week-1

---
+++ week-1/project.py
@@ -0,0 +1,30 @@
+r = 2
+n = 5
+t = 3
+p = 4
+
+# Simple interest
+
+a = r / 100
+b = x * t
+c = 1 + y 
+
+simpleInterest = p * z
+
+print(simpleInterest)
+
+# Compound interest
+
+x = r / n
+y = 1 + x
+z = y** (n * t)
+
+compoundInterest = p * z
+
+print(compoundInterest)
+
+# Annuity plan
+
+annuityPlan = p * ((z - 1) /x)
+
+print(annuityPlan)
